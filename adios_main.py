1d48a6096e75af90a175d7fd8bae4cfd11c47ce0
DESKTOP-NOF88F2\Lenovo <romangrism@gmail.com>
2024-04-29 12:02:06 +0900

    메인업데이트

--- adios_main.py
+++ adios_main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from fpdf import FPDF
 
 st.header("The Team Adios Portfolio :people_holding_hands:")
 
@@ -11,3 +12,9 @@ team_member = pd.DataFrame({
 })
 st.dataframe(team_member, hide_index=True, use_container_width=True)
 st.subheader(":two: Team Project")
+
+st.markdown("(1) 선용폼 구매 플랫폼 구축을 위한 타당성 분석 및 외국인 선원 체류시간별 코스/맛집 추천")
+st.video("./useData/Busan/busan_video.mp4")
+
+st.markdown("(2) 해외 축구팬들을 위한 OOF(Oracle Of Football) 플랫폼")
+st.video("./useData/OOF/OOF_Video.mp4")
