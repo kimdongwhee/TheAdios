40044bedcd236195c8bcf45e37d47cf5935658e9
DESKTOP-NOF88F2\Lenovo <romangrism@gmail.com>
2024-04-29 12:26:03 +0900

    업데이트

--- adios_main.py
+++ adios_main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import base64
 
 st.header("The Team Adios Portfolio :people_holding_hands:")
 
@@ -13,7 +14,19 @@ st.dataframe(team_member, hide_index=True, use_container_width=True)
 st.subheader(":two: Team Project")
 
 st.markdown("(1) 선용폼 구매 플랫폼 구축을 위한 타당성 분석 및 외국인 선원 체류시간별 코스/맛집 추천")
+with open("./useData/Busan/Busan_Final_PJT.pdf", "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+st.download_button(label="PDF 다운로드",
+                data=PDFbyte,
+                file_name="Busan_Final_PJT.pdf",
+                mime='application/octet-stream')
 st.video("./useData/Busan/busan_video.mp4")
 
 st.markdown("(2) 해외 축구팬들을 위한 OOF(Oracle Of Football) 플랫폼")
-st.video("./useData/OOF/OOF_Video.mp4")
+with open("./useData/OOF/OOF_Final_PJT.pdf", "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+st.download_button(label="PDF 다운로드",
+                data=PDFbyte,
+                file_name="OOF_Final_PJT.pdf",
+                mime='application/octet-stream')
+st.video("./useData/OOF/Final_OOF_VIDEO.mp4")
